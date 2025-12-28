f97d05c997b648163f638f3cd34b14d6fae52563
Lucas Bruner <lucas-bruner@hotmai.com.br>
2025-12-28 15:59:25 -0300

    add controller use case

--- src/main/java/br/com/fiap/lunchtech/core/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/lunchtech/core/controllers/UsuarioController.java
@@ -1,12 +1,19 @@
 package br.com.fiap.lunchtech.core.controllers;
 
 import br.com.fiap.lunchtech.core.dto.usuario.NovoUsuarioDTO;
+import br.com.fiap.lunchtech.core.dto.usuario.UsuarioAlteracaoDTO;
+import br.com.fiap.lunchtech.core.dto.usuario.UsuarioAlteradoDTO;
 import br.com.fiap.lunchtech.core.dto.usuario.UsuarioDTO;
+import br.com.fiap.lunchtech.core.dto.usuario.UsuarioSenhaDTO;
 import br.com.fiap.lunchtech.core.gateway.UsuarioGateway;
 import br.com.fiap.lunchtech.core.interfaces.IDataSource;
 import br.com.fiap.lunchtech.core.presenters.usuario.UsuarioPresenter;
+import br.com.fiap.lunchtech.core.usecases.usuario.AlterarSenhaUseCase;
+import br.com.fiap.lunchtech.core.usecases.usuario.AlterarUsuarioUseCase;
 import br.com.fiap.lunchtech.core.usecases.usuario.BuscarUsuariosPorNomeUseCase;
 import br.com.fiap.lunchtech.core.usecases.usuario.CadastrarUsuarioUseCase;
+import br.com.fiap.lunchtech.core.usecases.usuario.DeletarUsuarioUseCase;
+import br.com.fiap.lunchtech.core.usecases.usuario.ValidarLoginUseCase;
 
 import java.util.List;
 
@@ -40,5 +47,39 @@ public class UsuarioController {
         List<UsuarioDTO> usuariosPresenter = usuarios.stream().map(UsuarioPresenter::toDto).toList();
 
         return usuariosPresenter;
+    }
+
+    public UsuarioDTO alterarUsuario(UsuarioAlteracaoDTO usuarioAlteracaoDTO) {
+        var usuarioGateway = UsuarioGateway.create(this.dataStorageSource);
+        var useCaseAlterarUsuario = AlterarUsuarioUseCase.create(usuarioGateway);
+
+        var usuario = useCaseAlterarUsuario.run(usuarioAlteracaoDTO);
+        var usuarioPresenter = UsuarioPresenter.toDto(usuario);
+
+        return usuarioPresenter;
+    }
+
+    public void deletarUsuario(String login) {
+        var usuarioGateway = UsuarioGateway.create(this.dataStorageSource);
+        var useCaseDeletarUsuario = DeletarUsuarioUseCase.create(usuarioGateway);
+
+        useCaseDeletarUsuario.run(login);
+    }
+
+    public UsuarioAlteradoDTO alterarSenhaUsuario(UsuarioSenhaDTO usuarioSenhaDTO) {
+        var usuarioGateway = UsuarioGateway.create(this.dataStorageSource);
+        var useCaseAlterarSenhaUsuario = AlterarSenhaUseCase.create(usuarioGateway);
+
+        var usuario = useCaseAlterarSenhaUsuario.run(usuarioSenhaDTO);
+        var usuarioPresenter = UsuarioPresenter.mostrarUsuarioAlterado(usuario);
+
+        return usuarioPresenter;
+    }
+
+    public void validarLoginUsuario(UsuarioSenhaDTO usuarioSenhaDTO) {
+        var usuarioGateway = UsuarioGateway.create(this.dataStorageSource);
+        var useCaseValidarLogin = ValidarLoginUseCase.create(usuarioGateway);
+
+        useCaseValidarLogin.run(usuarioSenhaDTO);
     }
 }
