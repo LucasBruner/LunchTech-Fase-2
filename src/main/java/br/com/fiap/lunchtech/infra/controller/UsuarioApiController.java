5750fc364e33eb16b28feed3804a282c034fa3e9
Brenda Bernat <brendalouisebernat@gmail.com>
2025-12-29 15:37:49 -0300

    Rename controller class

--- src/main/java/br/com/fiap/lunchtech/infra/controller/UsuarioApiController.java
+++ src/main/java/br/com/fiap/lunchtech/infra/controller/UsuarioApiController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuarios")
-public class UsuarioController {
+public class UsuarioApiController {
 
 }
