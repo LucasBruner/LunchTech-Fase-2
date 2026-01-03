3619260211d046d641c9aae24bd43b2d11019fdd
Lucas Bruner <lucas-bruner@hotmai.com.br>
2026-01-03 13:45:05 -0300

    complete restaurante controller

--- src/main/java/br/com/fiap/lunchtech/core/controllers/RestauranteController.java
+++ src/main/java/br/com/fiap/lunchtech/core/controllers/RestauranteController.java
@@ -1,13 +1,17 @@
 package br.com.fiap.lunchtech.core.controllers;
 
 import br.com.fiap.lunchtech.core.dto.restaurante.NovoRestauranteDTO;
+import br.com.fiap.lunchtech.core.dto.restaurante.RestauranteAlteracaoDTO;
 import br.com.fiap.lunchtech.core.dto.restaurante.RestauranteDTO;
 import br.com.fiap.lunchtech.core.gateway.RestauranteGateway;
 import br.com.fiap.lunchtech.core.gateway.UsuarioGateway;
 import br.com.fiap.lunchtech.core.interfaces.IRestauranteDataSource;
 import br.com.fiap.lunchtech.core.interfaces.IUsuarioDataSource;
 import br.com.fiap.lunchtech.core.presenters.RestaurantePresenter;
+import br.com.fiap.lunchtech.core.usecases.restaurante.AlterarRestauranteUseCase;
+import br.com.fiap.lunchtech.core.usecases.restaurante.BuscarRestaurantePorNomeUseCase;
 import br.com.fiap.lunchtech.core.usecases.restaurante.CadastrarRestauranteUseCase;
+import br.com.fiap.lunchtech.core.usecases.restaurante.DeletarRestauranteUseCase;
 
 public class RestauranteController {
 
@@ -24,7 +28,7 @@ public class RestauranteController {
         return new RestauranteController(usuarioDataSource, restauranteDataSource);
     }
 
-    public RestauranteDTO cadastrar(NovoRestauranteDTO novoRestauranteDTO) {
+    public RestauranteDTO cadastrarRestaurante(NovoRestauranteDTO novoRestauranteDTO) {
         var restauranteGateway = RestauranteGateway.create(this.restauranteDataSource);
         var usuarioGateway = UsuarioGateway.create(this.usuarioDataSource);
         var useCaseNovoRestaurante = CadastrarRestauranteUseCase.create(restauranteGateway, usuarioGateway);
@@ -33,5 +37,33 @@ public class RestauranteController {
         var restaurantePresenter = RestaurantePresenter.toDto(novoRestaurante);
 
         return restaurantePresenter;
+    }
+
+    public RestauranteDTO buscarRestaurantePorNome(String nomeRestaurante) {
+        var restauranteGateway = RestauranteGateway.create(this.restauranteDataSource);
+        var useCaseBuscarPorNome = BuscarRestaurantePorNomeUseCase.create(restauranteGateway);
+
+        var restaurante = useCaseBuscarPorNome.run(nomeRestaurante);
+        var restaurantePresenter = RestaurantePresenter.toDto(restaurante);
+
+        return restaurantePresenter;
+    }
+
+    public RestauranteDTO alterarRestaurante(RestauranteAlteracaoDTO restauranteAlteracaoDTO) {
+        var restauranteGateway = RestauranteGateway.create(this.restauranteDataSource);
+        var usuarioGateway = UsuarioGateway.create(this.usuarioDataSource);
+        var useCaseAlterarRestaurante = AlterarRestauranteUseCase.create(restauranteGateway, usuarioGateway);
+
+        var restauranteAlterado = useCaseAlterarRestaurante.run(restauranteAlteracaoDTO);
+        var restaurantePresenter = RestaurantePresenter.toDto(restauranteAlterado);
+
+        return restaurantePresenter;
+    }
+
+    public void deletarRestaurante(String nomeRestaurante) {
+        var restauranteGateway = RestauranteGateway.create(this.restauranteDataSource);
+        var useCaseDeletarRestaurante = DeletarRestauranteUseCase.create(restauranteGateway);
+
+        useCaseDeletarRestaurante.run(nomeRestaurante);
     }
 }
