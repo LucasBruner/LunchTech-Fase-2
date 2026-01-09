dd027cd44dd4a7f4f9044f25d04698c4290152c0
Brenda Bernat <brendalouisebernat@gmail.com>
2026-01-08 21:08:45 -0300

    Delete unused DTO and service classes

--- src/main/java/br/com/fiap/lunchtech/infra/database/dto/tipoUsuario/NovoTipoUsuarioDTO.java
+++
@@ -1,4 +0,0 @@
-package br.com.fiap.lunchtech.infra.database.dto.tipoUsuario;
-
-public record NovoTipoUsuarioDTO(String tipoUsuario) {
-}
--- src/main/java/br/com/fiap/lunchtech/infra/database/dto/usuario/UsuarioDonoRestauranteDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.lunchtech.infra.database.dto.usuario;
-
-public record UsuarioDonoRestauranteDTO (String nomeUsuario,
-                                         String enderecoEmail,
-                                         String tipoDeUsuario,
-                                         String login){
-}
